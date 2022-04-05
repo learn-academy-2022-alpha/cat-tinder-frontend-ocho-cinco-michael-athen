b5e9289dc27d3635d4f4ce86b6655325818126bd
scalisema <scalisema@gmail.com>
2022-04-05 16:15:44 -0400

    20/20 Frontend Structure complete

---
+++ src/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./NotFound.css";
+
+class NotFound extends Component {
+    render() {
+        return (
+            <div className="Not_Found">
+                <h1>Not Found</h1>
+                <h2> You're Not Suppost to Be Here!!!! </h2>
+            </div>
+        )
+    }
+}
+export default NotFound
