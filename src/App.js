44a73141b1cde754deb2c746799850f41e5a5239
Athen Park <athenjpark@gmail.com>
2022-04-08 15:06:42 -0700

    Completed Fetch Challenges

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import CatNew from "./pages/CatNew";
 import CatShow from "./pages/CatShow";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import mockCats from './mockCats.js';
+
 import './App.css'
 import {
   BrowserRouter as Router,
@@ -20,17 +20,66 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      cats: mockCats
+      cats: []
     }
   }
 
-createCat = (cat) => {
-  console.log(cat)
+  componentDidMount(){
+    this.readCat()
+  }
+
+//CRUD Methods
+createCat = (newCat) => {
+  fetch("http://localhost:3000/cats", {
+    // converting an object to a string
+    body: JSON.stringify(newCat),
+    // specify the info being sent in JSON and the info returning should be JSON
+    headers: {
+      "Content-Type": "application/json"
+    },
+    // HTTP verb so the correct endpoint is invoked on the server
+    method: "POST"
+  })
+  .then(response => response.json())
+  .then(payload => this.readCat())
+  .catch(errors => console.log("Cat create errors:", errors))
+}
+
+readCat = () => {
+  fetch("http://localhost:3000/cats")
+  .then(response => response.json())
+  .then(payload => this.setState({cats: payload}))
+  .catch(errors => console.log("Cat read errors:", errors))
 }
 
 updateCat = (cat, id) =>{
-  console.log("cat:", cat)
-  console.log("id:", id)
+  fetch(`http://localhost:3000/cats/${id}`, {
+    // converting an object to a string
+    body: JSON.stringify(cat),
+    // specify the info being sent in JSON and the info returning should be JSON
+    headers: {
+      "Content-Type": "application/json"
+    },
+    // HTTP verb so the correct endpoint is invoked on the server
+    method: "PATCH"
+  })
+  .then(response => response.json())
+  .then(payload => this.readCat())
+  .catch(errors => console.log("Cat update errors:", errors))
+}
+
+deleteCat = (id) => {
+  fetch(`http://localhost:3000/cats/${id}`, {
+    // specify the info being sent in JSON and the info returning should be JSON
+    headers: {
+      "Content-Type": "application/json"
+    },
+    // HTTP verb so the correct endpoint is invoked on the server
+    method: "DELETE"
+  })
+  .then(response => response.json())
+  .then(payload => this.readCat())
+  .catch(errors => console.log("delete errors:", errors))
 }
 
   render() {
@@ -50,7 +99,7 @@ updateCat = (cat, id) =>{
               render={(props) => {
                 let id = props.match.params.id
                 let cat = this.state.cats.find(catObj => catObj.id === +id)
-                return <CatShow cat={cat} />
+                return <CatShow cat={cat} deleteCat={this.deleteCat}/>
               }}
             />
 
