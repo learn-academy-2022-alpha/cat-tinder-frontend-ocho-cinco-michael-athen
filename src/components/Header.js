db32f24a67463b13738950a86df84002ea71c1d6
Athen Park <athenjpark@gmail.com>
2022-04-11 13:14:52 -0700

    Added more styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import logo1 from "../assets/logo1.png";
+import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
     render() {
         return (
             <div className="Header">
             <h1>Cat Tinder</h1>
+            <div className="FlamePaw">
+            <img src={logo1} alt="page logo" height="60px" />
+            </div>
             </div>
         )
     }
